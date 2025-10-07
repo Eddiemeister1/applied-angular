d6b28f53e495a04590086ad94d9541ded42c2080
Eddiemeister1 <eduardodotel2@gmail.com>
2025-10-07 16:27:08 +0000

    Day 2 before lunch

---
+++ src/demos/pages/signals2.ts
@@ -0,0 +1,43 @@
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { GolfStore } from '../stores/golf';
+
+@Component({
+  selector: 'app-signals2',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: `
+    <p>Golfing with SignalStore</p>
+
+    @if (store.par() === null) {
+      <div class="join">
+        @for (par of store.golfPar; track par) {
+          <button
+            [disabled]="store.par() === par"
+            (click)="store.setPar(par)"
+            class="join-item btn btn-sm"
+          >
+            {{ par }}
+          </button>
+        }
+      </div>
+    } @else {
+      <p>Your Current Score is {{ store.strokeCount() }}</p>
+      <p>Your Par is {{ store.par() }}</p>
+
+      <div>
+        <button (click)="store.takeASwing()" class="btn btn-primary">
+          Take a Swing
+        </button>
+      </div>
+
+      <div>
+        <p>{{ store.getLevel() }}</p>
+      </div>
+    }
+  `,
+
+  styles: ``,
+})
+export class Signals2 {
+  store = inject(GolfStore);
+}
